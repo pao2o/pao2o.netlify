4ab6f10a39a5576f8b89926c2cc0b4557bfe37cf
pao2o <yugipao@gmail.com>
2023-06-25 11:43:06 -0600

    adjust delay

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function handleSectionClick(event) {
 
             targetSection.style.visibility = "visible";
             targetSection.classList.add("fade-in");
-        }, 2000); // Adjust the delay (in milliseconds) as needed
+        }, 500); // Adjust the delay (in milliseconds) as needed
     }
 }
 
