70119474d34e9803fc46971ec793eb93764cf138
Paolo Reyes <vreyes@hpsd.ca>
2023-07-06 15:28:05 -0600

    added utc timezone for consistent date picking

--- js/letf_backtest.js
+++ js/letf_backtest.js
@@ -223,11 +223,11 @@ function calculateLETFCAGR() {
 /*HELPER FUNCTIONS*/
 //format date in 'yyyy-MM-dd' format
 function formatDate(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}-${month}-${day}`;
-}
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+    const day = String(date.getUTCDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
 
 function standardDeviation(values) {
   const mean = values.reduce((acc, value) => acc + value, 0) / values.length;
