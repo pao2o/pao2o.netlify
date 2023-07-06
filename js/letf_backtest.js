5d8eda02b997b898417e579dab9156161795a02e
Paolo Reyes <vreyes@hpsd.ca>
2023-07-06 14:43:07 -0600

    created function to calculate period volatility

--- js/letf_backtest.js
+++ js/letf_backtest.js
@@ -29,84 +29,98 @@ function fetchDataFromDatabase(databasePath, query) {
 }
 
 function calculateAndUpdatePeriodLength() {
-    const startDateInput = document.querySelector('input[name="start_date"]');
-    const endDateInput = document.querySelector('input[name="end_date"]');
-    const periodLengthInput = document.querySelector('input[name="period_length"]');
-  
-    const startDate = new Date(startDateInput.value);
-    const endDate = new Date(endDateInput.value);
-  
-    const millisecondsPerDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
-    const daysDifference = Math.floor((endDate - startDate) / millisecondsPerDay);
-    const monthsDifference = daysDifference / 365.25; // Assuming each month has 30 days
-  
-    periodLengthInput.value = monthsDifference;
-  }
-  
+  const startDateInput = document.querySelector('input[name="start_date"]');
+  const endDateInput = document.querySelector('input[name="end_date"]');
+  const periodLengthInput = document.querySelector(
+    'input[name="period_length"]'
+  );
 
-  function calculatePeriodCAGR() {
-    console.log("test");
-    const startDateInput = document.querySelector('input[name="start_date"]');
-    const endDateInput = document.querySelector('input[name="end_date"]');
-    const periodLengthInput = document.querySelector('input[name="period_length"]');
-  
-    const startDate = new Date(startDateInput.value);
-    const endDate = new Date(endDateInput.value);
-    const periodLength = parseFloat(periodLengthInput.value);
-  
-    const query = `SELECT Dates, "1+daily return" FROM Data WHERE Dates BETWEEN '${formatDate(startDate)}' AND '${formatDate(endDate)}'`;
+  const startDate = new Date(startDateInput.value + "T00:00:00Z");
+  const endDate = new Date(endDateInput.value + "T00:00:00Z");
 
-    fetchDataFromDatabase("sql/letf_backtest.db", query)
-      .then((result) => {
-        // Process the query result
-        const values = result.map((row) => row[0]);
-        const values2 = result.map((row) => row[1]);
-        const returns = values.map((value) => parseFloat(value));
-    
-        const product = returns.reduce((acc, value) => acc * value, 1);
-        const cagr = Math.pow(product, 1 / periodLength) - 1;
-    
-        console.log("CAGR:", cagr);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }
-  
-  
-  // Helper function to format date in 'yyyy-MM-dd' format
-  function formatDate(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
-  
+  const millisecondsPerDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+  const daysDifference = Math.floor((endDate - startDate) / millisecondsPerDay);
+  const monthsDifference = daysDifference / 365.25; // Assuming each month has 30 days
 
+  periodLengthInput.value = monthsDifference.toFixed(2);
+}
 
+function calculatePeriodCAGR() {
+  console.log("test");
+  const startDateInput = document.querySelector('input[name="start_date"]');
+  const endDateInput = document.querySelector('input[name="end_date"]');
+  const periodLengthInput = document.querySelector(
+    'input[name="period_length"]'
+  );
+  const periodCAGRInput = document.querySelector('input[name="period_cagr"]');
 
+  const startDate = new Date(startDateInput.value + "T00:00:00Z");
+  const endDate = new Date(endDateInput.value + "T00:00:00Z");
+  const periodLength = parseFloat(periodLengthInput.value);
 
-/*
-fetchDataFromDatabase("sql/letf_backtest.db", "SELECT * FROM Data")
-  .then((result) => {
-    // Process the query result
-    result.forEach((row) => {
-      // Access row data, e.g., row[0], row[1], etc.
-      console.log(row);
+  const query = `SELECT "1+daily return" FROM Data WHERE Dates BETWEEN '${formatDate(
+    startDate
+  )}' AND '${formatDate(endDate)}'`;
+
+  fetchDataFromDatabase("sql/letf_backtest.db", query)
+    .then((result) => {
+      // Process the query result
+      const values = result.map((row) => row[0]);
+      const returns = values.map((value) => parseFloat(value));
+
+      const product = returns.reduce((acc, value) => acc * value, 1);
+      const cagr = Math.pow(product, 1 / periodLength) - 1;
+
+      periodCAGRInput.value = (cagr * 100).toFixed(2);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
-  })
-  .catch((error) => {
-    console.error("Error fetching database:", error);
-  });
-fetchDataFromDatabase("sql/letf_backtest.db", "SELECT * FROM selectData")
-  .then((result) => {
-    // Process the query result
-    result.forEach((row) => {
-      // Access row data, e.g., row[0], row[1], etc.
-      console.log(row);
+}
+
+function calculatePeriodVolatility() {
+  const startDateInput = document.querySelector('input[name="start_date"]');
+  const endDateInput = document.querySelector('input[name="end_date"]');
+  const periodVolatilityInput = document.querySelector(
+    'input[name="period_volatility"]'
+  );
+
+  const startDate = new Date(startDateInput.value + "T00:00:00Z");
+  const endDate = new Date(endDateInput.value + "T00:00:00Z");
+
+  const query = `SELECT "daily return" FROM Data WHERE Dates BETWEEN '${formatDate(
+    startDate
+  )}' AND '${formatDate(endDate)}'`;
+
+  fetchDataFromDatabase("sql/letf_backtest.db", query)
+    .then((result) => {
+      // Process the query result
+      const values = result.map((row) => row[0]);
+      const returns = values.map((value) => parseFloat(value));
+
+      const stdev = Math.sqrt(252) * standardDeviation(returns);
+      periodVolatilityInput.value = stdev.toFixed(2);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
-  })
-  .catch((error) => {
-    console.error("Error fetching database:", error);
-  });
-*/
+}
+
+/*HELPER FUNCTIONS*/
+//format date in 'yyyy-MM-dd' format
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
+function standardDeviation(values) {
+  const mean = values.reduce((acc, value) => acc + value, 0) / values.length;
+  const squaredDifferences = values.map((value) => (value - mean) ** 2);
+  const variance =
+    squaredDifferences.reduce((acc, value) => acc + value, 0) / values.length;
+  const stdev = Math.sqrt(variance);
+
+  return stdev;
+}
